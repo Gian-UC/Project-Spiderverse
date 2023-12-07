3658f9c16db40bf952ea481741a7ea434d4c98c8
Giancarlo Salomone <g.salomone@live.com>
2023-12-07 15:17:13 -0300

    adicionado o arquivo js e adicionado os eventos no botão do spiderman

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,1 +1,19 @@
-console.log("Olá");
+function handleMouseEnter() {
+  this.classList.add('s-card--hovered');
+}
+
+function handleMouseLeave(){
+  this.classList.remove('s-card--hovered');
+}
+
+function addEventListenersToCards() {
+  const cardElements = document.getElementsByClassName('s-card');
+  
+  for(let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index];
+    card.addEventListener('mouseenter', handleMouseEnter);
+    card.addEventListener('mouseleave', handleMouseLeave);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false)
