ed7a4bc5d11c34030005050e3001afdec826faf4
Giancarlo Salomone <g.salomone@live.com>
2023-12-08 23:23:18 -0300

    modificado os botões na home page onde estão os 3 spidermans e adicionado os efeitos de carousel

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,6 +20,17 @@ function addEventListenersToCards() {
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false)
 
-function selectCarouselItem(selectItem) {
-  console.log("Selecionando item', selectItem");
+function selectCarouselItem(selectedButtonElement) {
+  const selectedItem = selectedButtonElement.id;
+  const carousel = document.querySelector('.s-cards-carousel');
+  const transform = carousel.style.transform;
+  const rotateY = transform.match((/rotateY\((-?\d+deg)\)/i));
+  const rotateYDeg = -120 * (Number(selectedItem) - 1);
+  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+  carousel.style.transform = newTransform;
+
+  const activeButtonElement = document.querySelector('.s-controller__button--active');
+  activeButtonElement.classList.remove('s-controller__button--active');
+  selectedButtonElement.classList.add('s-controller__button--active');
 }
